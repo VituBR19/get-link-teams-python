f8b9f56d61a124184f6d70d1e391fe994da18908
Vitor Gabriel do Couto <59032828+VituBR19@users.noreply.github.com>
2020-08-19 16:30:26 -0300

    Organiza codigo

--- avabot.py
+++ avabot.py
@@ -4,9 +4,10 @@ import time
 import datetime
 
 class AvaBot:
-  def __init__(self, webdriver, login, pwd):
-    self.subjects = ['4945', '4950', '4952', '4940', '4930']
+  def __init__( self, webdriver, login, pwd ):
+    self.subjects = [ '4945', '4950', '4952', '4940', '4930' ]
     self.today = datetime.datetime.today().weekday()
+    
     self.teamsUrl = ''
 
     self.login = login
@@ -15,40 +16,42 @@ class AvaBot:
     self.driver = webdriver
 
   def AvaLogin(self): 
-    self.driver.get('https://ava.ead.unisal.br/login/index.php')
+    self.driver.get( 'https://ava.ead.unisal.br/login/index.php' )
     time.sleep(1)
 
     userName = self.driver.find_element_by_id('username')
     time.sleep(2)
-    userName.send_keys(self.login)
+    userName.send_keys( self.login )
 
     password = self.driver.find_element_by_id('password')
     time.sleep(2)
-    password.send_keys(self.pwd)
+    password.send_keys( self.pwd )
 
     loginBtn = self.driver.find_element_by_id('loginbtn')
     time.sleep(2)
     loginBtn.click()
+    
     time.sleep(1)
 
   def GetTeamsLink(self):
     self.AvaLogin()
     
-    todaySubject = self.subjects[self.today]
+    todaySubject = self.subjects[ self.today ]
 
     try:
-      self.driver.get(f'https://ava.ead.unisal.br/course/view.php?id={todaySubject}')
+      self.driver.get( f'https://ava.ead.unisal.br/course/view.php?id={todaySubject}' )
       
-      conferenceVid = self.driver.find_element_by_xpath(f"(//span[contains(text(), 'conferência') or contains(text(), 'conferencia') or contains(text(), 'Conferência')])[last()]")
-    
+      conferenceVid = self.driver.find_element_by_xpath( "(//span[contains(text(), 'conferência') or contains(text(), 'conferencia') or contains(text(), 'Conferência')])[last()]" )
       time.sleep(1)
+    
       conferenceVid.click()
       time.sleep(1)
 
-      self.teamsUrl = self.driver.find_element_by_xpath("//a[contains(text(), 'meetup-join')]").text 
+      self.teamsUrl = self.driver.find_element_by_xpath( "//a[contains(text(), 'meetup-join')]" ).text 
       time.sleep(2)
 
     except:
       self.teamsUrl = 'Desculpe, por algum motivo não consegui pegar o link. Reclame com quem me criokkk'
 
     return self.teamsUrl
+  
--- zapbot.py
+++ zapbot.py
@@ -15,6 +15,7 @@ class ZapzapBot:
     self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
 
     self.avabot = AvaBot(self.driver, self.login, self.pwd)
+    
     self.mensagem = self.avabot.GetTeamsLink()
 
     self.WatchChatForCommandLink()
@@ -29,10 +30,12 @@ class ZapzapBot:
     goToGrupo.click()
 
     grupo = self.driver.find_element_by_xpath("//span[@title='Teste de bot']").find_element_by_xpath('../../../..')
+    
     while( True ):
       chat = grupo.find_elements_by_class_name('_3ko75')
       
       print('waiting for link command...')
+      
       if(chat[1].text == '!link'):
         self.EnviarMensagem()
 
@@ -41,7 +44,9 @@ class ZapzapBot:
   def EnviarMensagem(self):
     chatBox = self.driver.find_element_by_class_name("_3uMse")
     chatBox.click()
+    
     print(self.mensagem)
+    
     chatBox.send_keys(self.mensagem)
 
     buttonSend = self.driver.find_element_by_xpath("//span[@data-icon='send']")
